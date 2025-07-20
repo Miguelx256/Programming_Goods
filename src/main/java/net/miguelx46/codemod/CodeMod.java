d477304db843707e609a1b28494b7b7f8ce91004
MiguelCM <carrizosam@uninorte.edu.co>
2025-07-20 16:14:36 -0500

    Item Java coffee creado (sin funcionalidad)

--- src/main/java/net/miguelx46/codemod/CodeMod.java
+++ src/main/java/net/miguelx46/codemod/CodeMod.java
@@ -47,7 +47,6 @@ public class CodeMod {
 
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
         @SubscribeEvent
