99323e83035a99017c0091a0210f04fcdf95b046
MiguelCM <carrizosam@uninorte.edu.co>
2025-07-20 10:40:06 -0500

    Commit test

--- src/main/java/net/miguelx46/codemod/CodeMod.java
+++ src/main/java/net/miguelx46/codemod/CodeMod.java
@@ -22,13 +22,10 @@ public class CodeMod {
     private static final Logger LOGGER = LogUtils.getLogger();
     public CodeMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
-
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
-
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
     }
@@ -42,7 +39,6 @@ public class CodeMod {
 
     }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
 
