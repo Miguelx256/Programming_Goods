a36c50d36ff980a992136f6c95828c839cd58f60
miguelx256 <miancasaplay@gmail.com>
2026-06-19 16:55:20 -0500

    crafteo de estructuras de datos funcionales

--- src/main/java/net/miguelx46/programminggoods/recipe/JavaCompilerRecipes.java
+++ src/main/java/net/miguelx46/programminggoods/recipe/JavaCompilerRecipes.java
@@ -4,6 +4,7 @@ import net.miguelx46.programminggoods.item.ModItems;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraftforge.items.ItemStackHandler;
+import net.minecraft.world.item.Items;
 
 public class JavaCompilerRecipes {
 
@@ -11,6 +12,22 @@ public class JavaCompilerRecipes {
 
         if (isJavaNodeRecipe(inventory)) {
             return new ItemStack(ModItems.JAVA_NODE.get());
+        }
+
+        if (isJavaArrayListRecipe(inventory)) {
+            return new ItemStack(ModItems.JAVA_ARRAYLIST.get());
+        }
+
+        if (isJavaHashMapRecipe(inventory)) {
+            return new ItemStack(ModItems.JAVA_HASHMAP.get());
+        }
+
+        if (isJavaStackRecipe(inventory)) {
+            return new ItemStack(ModItems.JAVA_STACK.get());
+        }
+
+        if (isJavaBinaryTreeRecipe(inventory)) {
+            return new ItemStack(ModItems.JAVA_BINARYTREE.get());
         }
 
         return ItemStack.EMPTY;
@@ -38,4 +55,97 @@ public class JavaCompilerRecipes {
 
                         inventory.getStackInSlot(8).isEmpty();
     }
+
+    private static boolean isJavaArrayListRecipe(ItemStackHandler inventory) {
+
+        return
+
+                inventory.getStackInSlot(0).is(Items.GOLD_INGOT) &&
+
+                        inventory.getStackInSlot(1).is(Items.GOLD_INGOT) &&
+
+                        inventory.getStackInSlot(2).is(Items.GOLD_INGOT) &&
+
+                        inventory.getStackInSlot(3).is(ModItems.JAVA_NODE.get()) &&
+
+                        inventory.getStackInSlot(4).is(ModItems.JAVA_NODE.get()) &&
+
+                        inventory.getStackInSlot(5).is(ModItems.JAVA_NODE.get()) &&
+
+                        inventory.getStackInSlot(6).is(Items.GOLD_INGOT) &&
+
+                        inventory.getStackInSlot(7).is(Items.GOLD_INGOT) &&
+
+                        inventory.getStackInSlot(8).is(Items.GOLD_INGOT);
+    }
+
+    private static boolean isJavaHashMapRecipe(ItemStackHandler inventory) {
+
+        return
+
+                inventory.getStackInSlot(0).is(Items.DIAMOND) &&
+
+                        inventory.getStackInSlot(1).is(Items.DIAMOND) &&
+
+                        inventory.getStackInSlot(2).is(Items.DIAMOND) &&
+
+                        inventory.getStackInSlot(3).is(ModItems.JAVA_NODE.get()) &&
+
+                        inventory.getStackInSlot(4).is(ModItems.JAVA_NODE.get()) &&
+
+                        inventory.getStackInSlot(5).is(ModItems.JAVA_NODE.get()) &&
+
+                        inventory.getStackInSlot(6).is(Items.DIAMOND) &&
+
+                        inventory.getStackInSlot(7).is(Items.DIAMOND) &&
+
+                        inventory.getStackInSlot(8).is(Items.DIAMOND);
+    }
+
+    private static boolean isJavaStackRecipe(ItemStackHandler inventory) {
+
+        return
+
+                inventory.getStackInSlot(0).is(Items.LAPIS_LAZULI) &&
+
+                        inventory.getStackInSlot(1).is(Items.LAPIS_LAZULI) &&
+
+                        inventory.getStackInSlot(2).is(Items.LAPIS_LAZULI) &&
+
+                        inventory.getStackInSlot(3).is(ModItems.JAVA_NODE.get()) &&
+
+                        inventory.getStackInSlot(4).is(ModItems.JAVA_NODE.get()) &&
+
+                        inventory.getStackInSlot(5).is(ModItems.JAVA_NODE.get()) &&
+
+                        inventory.getStackInSlot(6).is(Items.LAPIS_LAZULI) &&
+
+                        inventory.getStackInSlot(7).is(Items.LAPIS_LAZULI) &&
+
+                        inventory.getStackInSlot(8).is(Items.LAPIS_LAZULI);
+    }
+
+    private static boolean isJavaBinaryTreeRecipe(ItemStackHandler inventory) {
+
+        return
+
+                inventory.getStackInSlot(0).is(Items.EMERALD) &&
+
+                        inventory.getStackInSlot(1).is(Items.EMERALD) &&
+
+                        inventory.getStackInSlot(2).is(Items.EMERALD) &&
+
+                        inventory.getStackInSlot(3).is(ModItems.JAVA_NODE.get()) &&
+
+                        inventory.getStackInSlot(4).is(ModItems.JAVA_NODE.get()) &&
+
+                        inventory.getStackInSlot(5).is(ModItems.JAVA_NODE.get()) &&
+
+                        inventory.getStackInSlot(6).is(Items.EMERALD) &&
+
+                        inventory.getStackInSlot(7).is(Items.EMERALD) &&
+
+                        inventory.getStackInSlot(8).is(Items.EMERALD);
+    }
+
 }
