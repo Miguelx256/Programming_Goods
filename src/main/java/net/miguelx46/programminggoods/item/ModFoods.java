230b8467c4270c4a5ea5a7911e508a72ea16e41f
miguelx256 <miancasaplay@gmail.com>
2026-06-23 11:34:54 -0500

    ver 1.2, Balanceados crafteos y efectos de items consumibles

--- src/main/java/net/miguelx46/programminggoods/item/ModFoods.java
+++ src/main/java/net/miguelx46/programminggoods/item/ModFoods.java
@@ -6,17 +6,21 @@ import net.minecraft.world.food.FoodProperties;
 
 public class ModFoods {
     // items de comidas
-    public static final FoodProperties JAVA_JUICE = new FoodProperties.Builder().nutrition(9)
+    public static final FoodProperties JAVA_JUICE = new FoodProperties.Builder().nutrition(5)
             .saturationMod(0.5f).effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 4000, 2), 10f).build();
 
-    public static final FoodProperties JAVA_APPLE = new FoodProperties.Builder().nutrition(7)
-            .saturationMod(0.4f).effect(() -> new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 4300, 2), 10f).build();
+    public static final FoodProperties JAVA_APPLE = new FoodProperties.Builder().nutrition(8)
+            .saturationMod(0.4f)
+            .effect(() -> new MobEffectInstance(MobEffects.REGENERATION, 240, 2), 10f)
+            .effect(() -> new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 4300, 2), 10f)
+            .effect(() -> new MobEffectInstance(MobEffects.ABSORPTION, 4300, 12), 10f)
+            .build();
 
     public static final FoodProperties JAVA_ARRAYLIST = new FoodProperties.Builder()
             .alwaysEat()
-            .nutrition(8)
+            .nutrition(6)
             .saturationMod(0.4f)
-            .effect(() -> new MobEffectInstance(MobEffects.HEALTH_BOOST, 5500, 3), 10f)
+            .effect(() -> new MobEffectInstance(MobEffects.HEALTH_BOOST, 5500, 4), 10f)
             .build();
 
     public static final FoodProperties JAVA_HASHMAP = new FoodProperties.Builder()
@@ -29,7 +33,7 @@ public class ModFoods {
 
     public static final FoodProperties JAVA_STACK = new FoodProperties.Builder()
             .alwaysEat()
-            .nutrition(7)
+            .nutrition(6)
             .saturationMod(0.3f)
             .effect(() -> new MobEffectInstance(MobEffects.SLOW_FALLING, 3000, 1), 1.0f)
             .effect(() -> new MobEffectInstance(MobEffects.JUMP, 3000, 3), 1.0f)
@@ -39,7 +43,7 @@ public class ModFoods {
             .alwaysEat()
             .nutrition(6)
             .saturationMod(0.2f)
-            .effect(() -> new MobEffectInstance(MobEffects.ABSORPTION, 3500, 10), 1.0f)
+            .effect(() -> new MobEffectInstance(MobEffects.HEAL, 20, 4), 1.0f)
             .effect(() -> new MobEffectInstance(MobEffects.LUCK, 3500, 2), 1.0f)
             .build();
 
